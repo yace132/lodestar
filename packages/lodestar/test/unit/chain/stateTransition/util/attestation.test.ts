cb3a14c17dcb6de0be51d6a9be60c20b0dffd411
yace132 <10000reset@gmail.com>
2019-08-06 18:51:59 +0800

    Create attestation.test.ts
    Add test of getAttestationDataSlot

---
+++ packages/lodestar/test/unit/chain/stateTransition/util/attestation.test.ts
@@ -0,0 +1,24 @@
+import {expect} from "chai";
+import sinon from "sinon";
+
+import {config} from "@chainsafe/eth2.0-config/lib/presets/mainnet";
+import * as utils from "../../../../../../src/chain/stateTransition/util";
+
+import {generateState} from "../../../../utils/state";
+import {generateEmptyAttestation} from "../../../../utils/attestation";
+
+describe('getAttestationDataSlot',()=>{
+	let getCommitteeCountStub,getStartShardStub;
+	const sandbox = sinon.createSandbox();
+	beforeEach(() => {
+    	getCommitteeCountStub = sandbox.stub(utils, 'getCommitteeCount');
+	    getStartShardStub = sandbox.stub(utils, 'getStartShard');
+	})
+	const attestation = generateEmptyAttestation();
+	const state = generateState();
+	attestation.data.target.epoch = 10;
+	getCommitteeCountStub.returns(6400);
+	attestation.data.crosslink.shard = 123;
+	getStartShardStub.returns(0);
+	expect(getAttestationDataSlot(config,state,attestation.data)).to.be.equal(641);
+})
