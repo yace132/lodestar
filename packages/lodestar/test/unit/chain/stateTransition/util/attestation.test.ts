7a2733b198ab69b2ff4e8b95ff5c8054a25dd06a
yace132 <10000reset@gmail.com>
2019-08-12 15:19:14 +0800

    Update attestation.test.ts
    Correct the test

--- packages/lodestar/test/unit/chain/stateTransition/util/attestation.test.ts
+++ packages/lodestar/test/unit/chain/stateTransition/util/attestation.test.ts
@@ -2,23 +2,33 @@ import {expect} from "chai";
 import sinon from "sinon";
 
 import {config} from "@chainsafe/eth2.0-config/lib/presets/mainnet";
-import * as utils from "../../../../../../src/chain/stateTransition/util";
+import * as utils from "../../../../../src/chain/stateTransition/util";
 
 import {generateState} from "../../../../utils/state";
 import {generateEmptyAttestation} from "../../../../utils/attestation";
+import {getAttestationDataSlot} from "../../../../../src/chain/stateTransition/util";
 
 describe('getAttestationDataSlot',()=>{
 	let getCommitteeCountStub,getStartShardStub;
 	const sandbox = sinon.createSandbox();
+	
 	beforeEach(() => {
     	getCommitteeCountStub = sandbox.stub(utils, 'getCommitteeCount');
 	    getStartShardStub = sandbox.stub(utils, 'getStartShard');
 	})
-	const attestation = generateEmptyAttestation();
-	const state = generateState();
-	attestation.data.target.epoch = 10;
-	getCommitteeCountStub.returns(6400);
-	attestation.data.crosslink.shard = 123;
-	getStartShardStub.returns(0);
-	expect(getAttestationDataSlot(config,state,attestation.data)).to.be.equal(641);
+
+	afterEach(() => {
+    	sandbox.restore();
+  	});
+	
+	it(' should compute the correct slot',()=>{
+		const attestation = generateEmptyAttestation();
+		const state = generateState();
+		attestation.data.target.epoch = 10;
+		getCommitteeCountStub.returns(6400);
+		attestation.data.crosslink.shard = 123;
+		getStartShardStub.returns(0);
+		expect(getAttestationDataSlot(config,state,attestation.data)).to.be.equal(641);
+
+	})
 })
